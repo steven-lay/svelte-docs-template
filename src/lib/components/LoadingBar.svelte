3a8820e766327c17e67f5f3110202bca82f65aa9
Steven Lay <arbiter1000@gmail.com>
2021-09-04 14:30:50 +1000

    a

--- src/lib/components/LoadingBar.svelte
+++ src/lib/components/LoadingBar.svelte
@@ -1,36 +1,40 @@
-<script>
+<script lang="ts">
 	import { navigating } from '$app/stores';
 
-	let visible = false;
+	let showLoadingBar = false;
 
 	navigating.subscribe((nav) => {
-		visible = true ? nav != null : false;
+		if (nav?.from.path != nav?.to.path) {
+			showLoadingBar = true;
+		} else {
+			showLoadingBar = false;
+		}
 	});
 
-	function loadIn(node, { duration = 1500 }) {
+	function loadIn(node: Element, { duration = 1500 }) {
 		const style = getComputedStyle(node);
 		const width = parseFloat(style.width);
 
 		return {
 			duration,
-			css: (t) => `width: ${t * width}px;`
+			css: (t: number) => `width: ${t * width}px;`
 		};
 	}
 
-	function fade(node, { delay = 0, duration = 400 }) {
+	function loadOut(node: Element, { delay = 0, duration = 400 }) {
 		const style = getComputedStyle(node);
 		const opacity = +style.opacity;
 
 		return {
 			delay,
 			duration,
-			css: (t) => `opacity: ${t * opacity};` + `width: 100%`
+			css: (t: number) => `opacity: ${t * opacity};` + `width: 100%`
 		};
 	}
 </script>
 
-{#if visible}
-	<div class="loading-bar" in:loadIn out:fade={{ duration: 250 }} />
+{#if showLoadingBar}
+	<div class="loading-bar" in:loadIn out:loadOut={{ duration: 300 }} />
 {/if}
 
 <style>
